7a6eeeeb94984318555414a070ca52d1e25bf197
Benjamin MENANT <dev@menant-benjamin.fr>
2012-10-07 19:15:55 +0200

    First milestone over: displays video panes.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-
+$(function() {
+  var
+    // Functions declarations
+    click_cols_handler = function (evt) {
+      var
+        _id = this.id.charAt(this.id.length - 1),
+        $col = $('#video-col-' + _id, $video_wrapper)
+          .addClass('opened-column')
+          .removeClass('closed-column'),
+        $columns = $('div.column', $video_wrapper)
+          .not($col)
+          .addClass('closed-column')
+          .removeClass('opened-column');
+    };
+    // DOM elements declarations
+    $video_wrapper = $('#video-wrapper'),
+    $cols = $('article', $video_wrapper)
+      .click(click_cols_handler);
+});
