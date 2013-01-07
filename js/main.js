994adbcd664aa95c2f9a339e15b838b57765d449
Benjamin MENANT <dev@menant-benjamin.fr>
2013-01-08 00:21:37 +0100

    bugfixes

--- js/main.js
+++ js/main.js
@@ -565,4 +565,16 @@ $.fn.videoControlsMenu = function (_action) {
     $(window).trigger('hashchange');
   });
 
+  /**
+   * Share the love
+   */
+  $(function() {
+    var _hostname = document.location.protocol + '//' + document.location.hostname + document.location.pathname
+      , _hostname_trim = _hostname.replace(/index\.html/,'')
+      , _hostname_encoded = encodeURIComponent(_hostname_trim)
+      , $footer = $('footer ul')
+      , $fb = $('li.facebook > a', $footer).attr('href', 'http://www.facebook.com/sharer.php?s=100&p[title]=Chauss%E2%80%99%20ton%20univers%2C%20un%20documentaire%20Web%20marche%20sur%20la%20t%C3%AAte%20des%20sportifs&p[summary]=Le%20webdocumentaire%20%C2%ABChauss%E2%80%99ton%20univers%C2%BB%20est%20un%20projet%20universitaire%20r%C3%A9alis%C3%A9%20par%20dix%20%C3%A9tudiants%20en%20master%20multim%C3%A9dia%20%C3%A0%20l%E2%80%99ISIC%20(Institut%20des%20Sciences%20de%20l%E2%80%99Information%20et%20de%20la%20Communication)%20%C3%A0%20Bordeaux%203.%20Le%20principal%20sujet%20de%20ce%20webdocumentaire%20s%E2%80%99articule%20autour%20de%20la%20relation%20qu%E2%80%99entretient%20un%20sportif%20de%20haut%20niveau%20avec%20sa%20chaussure%20de%20sport.%20%C2%ABChauss%E2%80%99ton%20univers%C2%BB%20est%20un%20webdocumentaire%20constitu%C3%A9%20de%20vid%C3%A9os%20de%20sportifs%20en%20action%20et%20d%E2%80%99interviews%20de%20sportifs%20(pr%C3%A9cis%C3%A9ment%20dans%20le%20football%2C%20la%20danse%2C%20le%20rugby%2C%20la%20boxe%2C%20le%20roller)%20qui%20racontent%20leur%20exp%C3%A9rience%20avec%20leur%20chaussure%3A%20comment%20ils%20les%20ont%20choisi%2C%20comment%20ils%20les%20entretiennent%2C%20s%E2%80%99ils%20les%20recyclent%20ou%20non%E2%80%A6%20Des%20questions%20principalement%20ax%C3%A9es%20sur%20le%20c%C3%B4t%C3%A9%20affectif%20du%20sportif%20avec%20ses%20chaussures.%20Enfin%2C%20des%20s%C3%A9quences%20plus%20th%C3%A9oriques%20o%C3%B9%20des%20experts%20s%E2%80%99expriment%20sur%20ce%20sujet%20(pr%C3%A9cis%C3%A9ment%20un%20podologue%20et%20une%20psychanalyste).&p[url]=' + _hostname_encoded)
+    , $twttr = $('li.twitter > a', $footer).attr('href', 'https://twitter.com/intent/tweet?text=Chauss%E2%80%99%20ton%20univers%2C%20un%20documentaire%20Web%20marche%20sur%20la%20t%C3%AAte%20des%20sportifs%E2%80%AF!&url=' + _hostname_encoded);
+  });
+
 })(jQuery);
